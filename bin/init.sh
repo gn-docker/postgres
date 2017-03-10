e72bd31d53ba4eafa32b2413c6f24d28a7b6b388
Dmitry Mozzherin <dmozzherin@gmail.com>
2017-03-10 06:09:40 -0600

    change config

--- bin/init.sh
+++ bin/init.sh
@@ -3,14 +3,14 @@ set -e
 
 function init_user {
 
-# gosu root adduser $DB_USER --gecos "" --disabled-login
+# gosu root adduser $DB_USERNAME --gecos "" --disabled-login
 
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER <<-EOSQL
-  CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+  CREATE USER $DB_USERNAME WITH PASSWORD '$DB_PASSWORD';
   CREATE DATABASE $DB_DATABASE;
-  GRANT ALL PRIVILEGES ON DATABASE $DB_DATABASE TO $DB_USER;
+  GRANT ALL PRIVILEGES ON DATABASE $DB_DATABASE TO $DB_USERNAME;
 EOSQL
 
 }
 
-id "$DB_USER" >/dev/null 2>&1 || init_user
+id "$DB_USERNAME" >/dev/null 2>&1 || init_user
